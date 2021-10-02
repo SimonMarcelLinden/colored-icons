ce08b31a1306a461239a9bd57a87274d08b5926f
SimonMarcelLinden <info@snerve.de>
2021-10-02 22:33:31 +0200

    Font icon for linkedin

---
+++ src/components/linkedin/linkedin.icon.ts
@@ -0,0 +1,12 @@
+import Vue from "vue";
+
+export default /*#__PURE__*/ Vue.extend({
+	name: "colored-linkedin",
+	components: {},
+	props: {},
+	data() {
+		return {};
+	},
+	computed: {},
+	methods: {}
+});
---
+++ src/components/linkedin/linkedin.icon.vue
@@ -0,0 +1,9 @@
+<template>
+	<div class="colored-linkedin">
+		<i class="colored-icon-social-linkedin-background"></i>
+		<i class="colored-icon-social-linkedin-font"></i>
+	</div>
+</template>
+
+<script src="./linkedin.icon.ts" lang="ts"></script>
+<style src="./linkedin.icon.css" lang="css"/>
