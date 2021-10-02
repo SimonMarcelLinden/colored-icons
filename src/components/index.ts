c68a6019c742c5f1b7ad8411efb5bcf0e6c15ed4
SimonMarcelLinden <info@snerve.de>
2021-10-03 00:27:34 +0200

    Export font icons

--- src/components/index.ts
+++ src/components/index.ts
@@ -4,4 +4,6 @@ export { default as ColoredInstagram } from '@/components/instagram/instagram.ic
 export { default as ColoredLinkedin } from '@/components/linkedin/linkedin.icon.vue';
 export { default as ColoredPinterest } from '@/components/pinterest/pinterest.icon.vue';
 export { default as ColoredTicktock } from '@/components/ticktock/ticktock.icon.vue';
+export { default as ColoredTwitter } from '@/components/twitter/twitter.icon.vue';
+export { default as ColoredWhatsapp } from '@/components/whatsapp/whatsapp.icon.vue';
 
