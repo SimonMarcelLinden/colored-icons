4b5250d52c8f52ed0280364afd542e8b7ad60a8c
SimonMarcelLinden <info@snerve.de>
2021-10-03 00:25:21 +0200

    Font icon for twitter

---
+++ src/components/twitter/twitter.icon.vue
@@ -0,0 +1,9 @@
+<template>
+	<div class="colored-ticktock">
+		<i class="colored-icon-social-twitter-background"></i>
+		<i class="colored-icon-social-twitter-bird"></i>
+	</div>
+</template>
+
+<script src="./twitter.icon.ts" lang="ts"></script>
+<style src="./twitter.icon.css" lang="css"/>
