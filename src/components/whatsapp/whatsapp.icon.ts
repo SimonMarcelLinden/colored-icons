75592c8b3eb2ebe4e6286853a0451bb030afbb60
SimonMarcelLinden <info@snerve.de>
2021-10-03 00:26:01 +0200

    Font icon for whatsapp

---
+++ src/components/whatsapp/whatsapp.icon.ts
@@ -0,0 +1,12 @@
+import Vue from "vue";
+
+export default /*#__PURE__*/ Vue.extend({
+	name: "colored-whatsapp",
+	components: {},
+	props: {},
+	data() {
+		return {};
+	},
+	computed: {},
+	methods: {}
+});
---
+++ src/components/whatsapp/whatsapp.icon.vue
@@ -0,0 +1,11 @@
+<template>
+	<div class="colored-whatsapp">
+		<i class="colored-icon-social-whatsapp-background"></i>
+		<i class="colored-icon-social-whatsapp-phone"></i>
+		<i class="colored-icon-social-whatsapp-ring"></i>
+		<i class="colored-icon-social-whatsapp-shadow"></i>
+	</div>
+</template>
+
+<script src="./whatsapp.icon.ts" lang="ts"></script>
+<style src="./whatsapp.icon.css" lang="css"/>
