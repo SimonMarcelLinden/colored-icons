f1709b2ea7f52b614948674f8a127ea6c8fd3dcf
SimonMarcelLinden <info@snerve.de>
2021-10-03 11:58:31 +0200

    Import scss for icon fonts

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,5 @@
+import '@/fonts/colored-icon.scss';
+
 /* eslint-disable import/prefer-default-export */
 export { default as ColoredFacebook } from '@/components/facebook/facebook.icon.vue';
 export { default as ColoredInstagram } from '@/components/instagram/instagram.icon.vue';
